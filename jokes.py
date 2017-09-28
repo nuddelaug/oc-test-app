3779fd3c16ded0b0fe881a08c6407db7098a2af2
Michael Lang <michael.lang@ctbto.org>
2017-09-28 14:11:01 +0200

    * changed to flask

--- jokes.py
+++ jokes.py
@@ -1,9 +1,44 @@
 import json
 from random import SystemRandom as Random
+from flask import Flask, jsonify
+from flask_autodoc.autodoc import Autodoc
+import os
 try:    jokes = json.load(open('jn.json'))
 except: jokes = {'value': ['Error opening database']}
 
-def application(environment, start_response):
-    start_response('200 OK', [('Content-Type', 'application/json')])
-    Random().shuffle(jokes['value'])
-    return jokes['value'][0]
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+Autodoc(app)
+
+@app.route('/health/', methods=['GET'])
+@app.doc()
+def health():
+    """Health check entrypoint"""
+    jl = len(jokes.get('value'))
+    if jl > 1:
+        return jsonify(dict(status='ok', message='%s entries in joke book' % len(jl)))
+    # fail if not true
+
+@app.route('/', methods=['GET'])
+def documentation():
+    """Documentation access"""
+    return auto.html(
+                     title='API endpoint documentation',
+                     author='Michael Lang <Michael.Lang@ctbto.org>',)
+
+@app.route('/joke/random/', methods=['GET', 'POST'])
+@app.doc()
+def get_joke():
+    Random().shuffle(jokes.get('value'))
+    jl = len(jokes.get('value'))
+    if request.method == 'GET':
+        return jsonify(dict(status='ok', joke=jokes.get('value')[0]))
+    elif request.method == 'POST':
+        limit = request.args.get('limit', 1)
+        if limit > jl:  limit = jl
+        return jsonify(dict(status='ok', joke=jokes.get('value')[:limit]))
+
+if __name__ == '__main__':
+    app.debug = False
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, threaded=True)
--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 setup (
     name             = "jokes",
-    version          = "0.1",
+    version          = "0.2",
     description      = "Example application providing Juck Norris jokes",
     packages         = find_packages(),
-    install_requires = ["gunicorn"],
+    install_requires = ["flask", "flask_autodoc"],
 )
