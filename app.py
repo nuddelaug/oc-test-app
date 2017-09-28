a505e76aacc949cf625a6b11334914cbe2c8249c
Michael Lang <michael.lang@ctbto.org>
2017-09-28 15:19:13 +0200

    * added category selection * changed random return if category selected

--- app.py
+++ app.py
@@ -35,8 +35,13 @@ def get_joke():
         return jsonify(dict(status='ok', joke=jokes.get('value')[0]))
     elif request.method == 'POST':
         limit = request.args.get('limit', 1)
+        category = request.args.get('category', False)
         if limit > jl:  limit = jl
-        return jsonify(dict(status='ok', joke=jokes.get('value')[:limit]))
+        if category == False:
+            return jsonify(dict(status='ok', joke=jokes.get('value')[:limit]))
+        content = filter(lambda x: category in x['categories'], jokes.get('value'))
+        if limit > len(content):    limit = len(content)
+        return jsonify(dict(status='ok', joke=content[:limit]))
 
 if __name__ == '__main__':
     app.debug = False
